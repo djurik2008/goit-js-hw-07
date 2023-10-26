10bbbf817ff13d6c220d8be634a8807af5595814
Oleh Tymoshenko <djurik2008@gmail.com>
2023-10-26 15:05:27 +0300

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onImgClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({preview, original, description}) =>
+        `<li class="gallery__item">
+   <a class="gallery__link"
+   href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
